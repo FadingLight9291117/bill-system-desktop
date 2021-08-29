ede05df3a3967bbaae6c3fa34ca11a679c11ab85
clz <fadinglight9291117@126.com>
2021-08-29 15:52:24 +0800

    tmp

---
+++ config/config.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const yaml = require('js-yaml');
+const path = require('path');
+
+const configFile = path.join(__dirname, 'config.yaml')
+
+const data = fs.readFileSync(configFile, "utf8")
+const config = yaml.load(data)
+
+exports.config = config
+
