6fb4ae6b7f681370332a3a1231e1609742dcbf84
clz <fadinglight9291117@126.com>
2021-09-07 22:12:26 +0800

    重构了项目结构，准备加个remote mongo sync

--- data/index.js
+++ data/index.js
@@ -1,6 +1,9 @@
 const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
 
-const db = new sqlite3.Database('data.db');
+const dbPath = path.join(__dirname, 'data.db');
+
+const db = new sqlite3.Database(dbPath);
 
 function initDb() {
     const createTable = `
